e928193a0bf5dc4e868a4130ac6ff53cd5f2be2b
bmw7788 <70685855+bmw7788@users.noreply.github.com>
2024-09-02 11:44:13 +0800

    Update run.py

--- run.py
+++ run.py
@@ -19,8 +19,8 @@ def ssh_multiple_connections(hosts_info, command):
             user = stdout.read().decode().strip()
             users.append(user)
             hostnames.append(hostname)
-            cmd = 'cd ~/domains/alist && ~/.npm-global/bin/pm2 start ./alist -- server'
-            stdin, stdout, stderr = ssh.exec_command(cmd)
+            stdin, stdout, stderr = ssh.exec_command('cd ~/domains/alist')
+            stdin, stdout, stderr = ssh.exec_command('~/.npm-global/bin/pm2 start ./alist -- server')
             ssh.close()
         except Exception as e:
             print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")

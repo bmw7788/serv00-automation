260e59a375c1e273a563931e39effe4b1c41bd82
bmw7788 <70685855+bmw7788@users.noreply.github.com>
2024-09-02 12:21:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -19,7 +19,6 @@ def ssh_multiple_connections(hosts_info, command):
             user = stdout.read().decode().strip()
             users.append(user)
             hostnames.append(hostname)
-            ssh.exec_command('cd ~/domains/alist;./alist server')
             ssh.close()
         except Exception as e:
             print(f"用户：{username}，连接 {hostname} 时出错: {str(e)}")
@@ -28,14 +27,5 @@ def ssh_multiple_connections(hosts_info, command):
 ssh_info_str = os.getenv('SSH_INFO', '[]')
 hosts_info = json.loads(ssh_info_str)
 
-command = 'whoami'
+command = 'cd ~/domains/alist;./alist server'
 user_list, hostname_list = ssh_multiple_connections(hosts_info, command)
-user_num = len(user_list)
-content = "SSH服务器登录信息：\n"
-for user, hostname in zip(user_list, hostname_list):
-    content += f"用户名：{user}，服务器：{hostname}\n"
-beijing_timezone = timezone(timedelta(hours=8))
-time = datetime.now(beijing_timezone).strftime('%Y-%m-%d %H:%M:%S')
-menu = requests.get('https://api.zzzwb.com/v1?get=tg').json()
-loginip = requests.get('https://api.ipify.org?format=json').json()['ip']
-content += f"本次登录用户共： {user_num} 个\n登录时间：{time}\n登录IP：{loginip}"
